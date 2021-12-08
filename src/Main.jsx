35219c3abe05c8ae6685fe92955990332dd18d3b
Drew Payton <payton.drew@gmail.com>
2021-12-08 13:36:23 -0700

    Add destination search box & state to keep track of weather a origin and destination city has been selected from google results

--- src/Main.jsx
+++ src/Main.jsx
@@ -8,16 +8,33 @@ import './App.css';
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { address: '' };
+    this.state = {
+      originCity: '',
+      destinationCity: '',
+      originSelected: false,
+      destinationSelected: false
+    };
   }
 
-  handleChange = address => {
-    this.setState({ address });
+  handleOriginChange = originCity => {
+    this.setState({ originCity, originSelected: false });
   };
 
-  handleSelect = address => {
-    this.setState({address: address});
-    geocodeByAddress(address)
+  handleDestinationChange = destinationCity => {
+    this.setState({ destinationCity, destinationSelected: false});
+  };
+
+  handleOriginSelect = originCity => {
+    this.setState({originCity, originSelected: true});
+    geocodeByAddress(originCity)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error));
+  };
+
+  handleDestinationSelect = destinationCity => {
+    this.setState({destinationCity, destinationSelected: true});
+    geocodeByAddress(destinationCity)
       .then(results => getLatLng(results[0]))
       .then(latLng => console.log('Success', latLng))
       .catch(error => console.error('Error', error));
@@ -31,45 +48,87 @@ class Main extends React.Component {
     }
 
     return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-        searchOptions={searchOptions}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <input
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input',
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
+      <div>
+        <PlacesAutocomplete
+          value={this.state.originCity}
+          onChange={this.handleOriginChange}
+          onSelect={this.handleOriginSelect}
+          searchOptions={searchOptions}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: 'Search Places ...',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className="autocomplete-dropdown-container">
+                {loading && <div>Loading...</div>}
+                {suggestions.map(suggestion => {
+                  const className = suggestion.active
+                    ? 'suggestion-item--active'
+                    : 'suggestion-item';
+                  // inline style for demonstration purpose
+                  const style = suggestion.active
+                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        )}
-      </PlacesAutocomplete>
+          )}
+        </PlacesAutocomplete>
+
+        <PlacesAutocomplete
+          value={this.state.destinationCity}
+          onChange={this.handleDestinationChange}
+          onSelect={this.handleDestinationSelect}
+          searchOptions={searchOptions}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: 'Search Places ...',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className="autocomplete-dropdown-container">
+                {loading && <div>Loading...</div>}
+                {suggestions.map(suggestion => {
+                  const className = suggestion.active
+                    ? 'suggestion-item--active'
+                    : 'suggestion-item';
+                  // inline style for demonstration purpose
+                  const style = suggestion.active
+                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+      </div>
     );
   }
 }
